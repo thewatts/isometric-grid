498b14f80e1df6d3fc93d86d7ee0d99364e406ad
Nathaniel Watts <reg@nathanielwatts.com>
2023-07-10 22:45:50 -0500

    Clean code + use I/J hat in calculations
    Fantastic Video: https://www.youtube.com/watch?v=kYB8IZa5AuE

--- app/main.rb
+++ app/main.rb
@@ -1,67 +1,76 @@
+IHAT_X = 1
+IHAT_Y = 0.5
+JHAT_X = -1
+JHAT_Y = 0.5
+
 def tick(args)
   args.gtk.reset if args.inputs.keyboard.key_down.r
 
   set_defaults(args)
 
-  process_input(args)
+  handle_input(args)
+  render_tiles(args)
+  render_instructions(args)
 
-  tiles = build_tiles(args)
-
-  args.outputs.background_color = [30, 41, 59]
-  args.outputs.sprites << tiles
-
-  debug(args)
-  instructions(args)
+  render_debug(args)
 end
 
 def set_defaults(args)
-  args.state.tile_animation_offset_factor ||= 1
-  args.state.animation_magnitude ||= 1
+  args.state.tile_animation_offset_factor ||= 0
+  args.state.animation_magnitude ||= 0
   args.state.scale ||= 1
   args.state.row_count ||= 1
   args.state.column_count ||= 1
+
+  args.outputs.background_color = [30, 41, 59]
+end
+
+def render_tiles(args)
+  args.state.tiles = build_tiles(args)
+
+  args.outputs.sprites << args.state.tiles
 end
 
 def build_tiles(args)
-  tile_animation_offset_factor = args.state.tile_animation_offset_factor
-  animation_magnitude = args.state.animation_magnitude
   tick_count = args.state.tick_count
-  scale = args.state.scale
-  column_count = args.state.column_count
-  row_count = args.state.row_count
+
+  animation_magnitude = args.state.animation_magnitude
+  tile_animation_offset_factor = args.state.tile_animation_offset_factor
 
   sprite_width = 32
   sprite_height = 32
   sprite_path = "sprites/boxes/iso-box-slate.png"
 
-  grid_size = row_count * column_count
+  center = {x: args.grid.w.half, y: args.grid.h.half}
 
-  tile_width = sprite_width * scale
-  tile_height = sprite_height * scale
+  scale = args.state.scale
+  column_count = args.state.column_count
+  row_count = args.state.row_count
 
-  center = args.grid.w.half
-  middle = args.grid.h.half
+  tile_grid_size = row_count * column_count
+  tile_size = {w: sprite_width * scale, h: sprite_height * scale}
 
-  starting_x = center - tile_width.half
-  starting_y = middle - tile_height.half - (tile_height.half * row_count).half
+  x_origin = center.x - tile_size.w.half
+  y_origin = center.y - tile_size.h.half - (tile_size.h * row_count).quarter
 
-  tiles = grid_size.times.to_a.map do |tile_number|
+  tile_grid_center = {x: x_origin, y: y_origin}
+
+  args.state.tile_grid_center = tile_grid_center
+
+  tiles = tile_grid_size.times.to_a.map do |tile_number|
     column = tile_number % column_count
     row = tile_number.idiv(row_count)
 
-    x = starting_x + (tile_width.half * column) - (tile_width.half * row)
-    y = starting_y + (tile_height.quarter * row) + (tile_height.quarter * column)
+    translated_coordinates = to_screen_coordinate(column: column, row: row, tile_size: tile_size)
 
     timing = tick_count + (tile_number * tile_animation_offset_factor)
     animation_offset = (Math.sin(timing.fdiv(10)) * animation_magnitude)
 
-    y += animation_offset
-
     {
-      x: x,
-      y: y,
-      w: sprite_width * scale,
-      h: sprite_height * scale,
+      x: x_origin + translated_coordinates.x,
+      y: y_origin + translated_coordinates.y + animation_offset,
+      w: tile_size.w,
+      h: tile_size.h,
       source_w: sprite_width,
       source_h: sprite_height,
       path: sprite_path,
@@ -72,16 +81,35 @@ def build_tiles(args)
   first_tile = tiles.first
   first_tile[:path] = "sprites/boxes/iso-box-orange.png"
 
+  args.outputs.solids << tiles if args.state.debug_grid
+
   # found_tiles = tiles.reverse.select do |tile|
   #   tile.intersect_rect?(args.inputs.mouse)
   # end
 
-  # found_tiles.last[:path] = "sprites/boxes/iso-box-orange.png" if found_tiles.any?
+  # found_tile = found_tiles.last
+
+  # if found_tile
+  #   # $gtk.notify! "Tile Number: #{found_tile.x}"
+  #   $gtk.set_system_cursor("hand")
+
+  #   # found_tile.y += 2 * scale
+  #   found_tile.path = "sprites/boxes/iso-box-orange.png"
+  # else
+  #   $gtk.set_system_cursor("arrow")
+  # end
 
   tiles.reverse
 end
 
-def process_input(args)
+def to_screen_coordinate(column:, row:, tile_size:)
+  {
+    x: column * IHAT_X * 0.5 * tile_size.w + row * JHAT_X * 0.5 * tile_size.w,
+    y: column * IHAT_Y * 0.5 * tile_size.h + row * JHAT_Y * 0.5 * tile_size.h
+  }
+end
+
+def handle_input(args)
   if args.inputs.keyboard.key_down.up
     args.state.animation_magnitude += 1
   elsif args.inputs.keyboard.key_down.down
@@ -93,11 +121,13 @@ def process_input(args)
     args.state.tile_animation_offset_factor += 1
   elsif args.inputs.keyboard.key_down.left
     return if args.state.tile_animation_offset_factor <= 1
+
     args.state.tile_animation_offset_factor -= 1
   end
 
   if args.inputs.keyboard.key_down.minus
     return if args.state.scale <= 1
+
     args.state.scale -= 1
   elsif args.inputs.keyboard.key_down.equal_sign
     args.state.scale += 1
@@ -105,8 +135,8 @@ def process_input(args)
 
   if args.inputs.keyboard.key_down.zero
     args.state.scale = 1
-    args.state.tile_animation_offset_factor = 1
-    args.state.animation_magnitude = 1
+    args.state.tile_animation_offset_factor = 0
+    args.state.animation_magnitude = 0
   end
 
   if args.inputs.keyboard.key_down.close_square_brace
@@ -117,9 +147,13 @@ def process_input(args)
     args.state.row_count -= 1
     args.state.column_count -= 1
   end
+
+  if args.inputs.keyboard.key_down.tab
+    args.state.debug_grid = !args.state.debug_grid
+  end
 end
 
-def debug(args)
+def render_debug(args)
   args.outputs.labels << new_debug_line(top: 30, text: "FPS: #{args.gtk.current_framerate.to_sf}")
   args.outputs.labels << new_debug_line(top: 50, text: "Animation Offset: #{args.state.tile_animation_offset_factor}")
   args.outputs.labels << new_debug_line(top: 70, text: "Animation Magnitude: #{args.state.animation_magnitude}")
@@ -129,13 +163,31 @@ def debug(args)
   background_height = 114
   background_top_padding = background_height + 25
   args.outputs.primitives << {x: 10.from_left, y: background_top_padding.from_top, w: 195, h: background_height, r: 30, g: 41, b: 59, a: 200, primitive_marker: :solid}
+
+  if args.state.debug_grid
+    top = args.grid.top
+    bottom = args.grid.bottom
+    left = args.grid.left
+    right = args.grid.right
+
+    center = {x: args.grid.w.half, y: args.grid.h.half}
+
+    args.outputs.lines << [center.x, bottom, center.x, top, 255, 255, 255, 90]
+    args.outputs.lines << [left, center.y, right, center.y, 255, 255, 255, 90]
+
+    tile_grid_center = args.state.tile_grid_center
+
+    yellow = [253, 224, 71, 255]
+    args.outputs.lines << [tile_grid_center.x, bottom, tile_grid_center.x, top, *yellow]
+    args.outputs.lines << [left, tile_grid_center.y, right, tile_grid_center.y, *yellow]
+  end
 end
 
 def new_debug_line(top:, text:)
   [20.from_left, top.from_top, text, 0, 0, 255, 255, 255, 255, "fonts/OpenSans-MediumItalic.ttf"]
 end
 
-def instructions(args)
+def render_instructions(args)
   args.outputs.labels << new_instruction_line(top: 30, text: "-/= to change scale")
   args.outputs.labels << new_instruction_line(top: 45, text: "[ or ] to change grid size")
   args.outputs.labels << new_instruction_line(top: 60, text: "Up/Down to change animation magnitude")
